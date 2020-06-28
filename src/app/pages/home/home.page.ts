f7402528907ce0544baf846bd5d020e54ea39faa
Martin Molinares Araujo <memolinares@uninorte.edu.co>
2020-06-27 20:52:49 -0500

    First Commit

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  components: ComponentI[] = [
+    {
+      icon: 'american-football',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'logo-google-playstore',
+      name: 'Alert',
+      redirectTo: '/alert'
+    }
+  ] 
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+interface ComponentI {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
