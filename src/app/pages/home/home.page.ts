585f92f65cd125100b91abcb543647c7af7d54f9
Martin Molinares Araujo <memolinares@uninorte.edu.co>
2020-06-28 14:01:37 -0500

    feat: card comÃponent

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -27,6 +27,11 @@ export class HomePage implements OnInit {
       icon: 'radio-button-on',
       name: 'Buttons and Router',
       redirectTo: '/buttons'
+    },
+    {
+      icon: 'card',
+      name: 'Cards',
+      redirectTo: '/card'
     }
   ] 
 
