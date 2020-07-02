3187fbc2cf99451eeac2f9df5515591ba7822528
Martin Molinares Araujo <memolinares@uninorte.edu.co>
2020-07-01 19:08:19 -0500

    feat: popover component

---
+++ src/app/pages/popover/popover.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+import { PopinfoComponent } from 'src/app/components/popinfo/popinfo.component';
+
+@Component({
+  selector: 'app-popover',
+  templateUrl: './popover.page.html',
+  styleUrls: ['./popover.page.scss'],
+})
+export class PopoverPage implements OnInit {
+
+  constructor(private popoverCtrl: PopoverController) { }
+
+  ngOnInit() {
+  }
+
+  async showPop(event){
+    const popover = await this.popoverCtrl.create({
+      component: PopinfoComponent,
+      event,
+      mode: 'ios',
+      backdropDismiss: true
+    });
+
+    await popover.present();
+
+    // const { data } = await popover.onDidDismiss();
+    const { data } = await popover.onWillDismiss();
+        
+
+    console.log(data)
+  }
+
+}
